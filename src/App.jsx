758e50d036512085909b9b9cb1ef0f89385972c2
OwenLobato <owenlobatov@gmail.com>
2023-05-04 13:03:47 -0600

    Add get and update user and add messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
-import { collection, addDoc, getDocs, doc, deleteDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs, doc, deleteDoc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "./firebaseConfig.js";
 import { MESSAGES } from "./MESSAGES.js";
 
 function App() {
 
+  const [editMode, setEditMode] = useState(false);
+  const [userId, setUserId] = useState("");
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [users, setUsers] = useState([]);
@@ -22,26 +24,61 @@ function App() {
       await addDoc(collection(db, "agenda"), user)
         .then(alert(MESSAGES.USER_CREATED))
     } catch (e) {
-      console.error("Error adding document: ", e);
+      alert(MESSAGES.ERROR.USER_NOT_CREATED, e.message);
     }
     setErrorMsg(null);
     setName("");
     setPhone("");
   };
-  
+
   const getUsers = async () => {
     const { docs } = await getDocs(collection(db, "agenda"));
     const newArray = docs.map(item => ({ id: item.id, ...item.data() }));
     setUsers(newArray);
   }
-  
+
   const deleteUser = async (id) => {
     try {
-      await deleteDoc(doc(db, "agenda", id)); 
+      await deleteDoc(doc(db, "agenda", id))
+        .then(alert(MESSAGES.USER_DELETED));
     } catch (error) {
       console.log(error);
     }
-    
+  }
+
+  const getUser = async (id) => {
+    const docRef = doc(db, "agenda", id);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const { userName, userPhone } = docSnap.data();
+      setUserId(id);
+      setName(userName);
+      setPhone(userPhone);
+      setEditMode(true);
+      alert(MESSAGES.USER_FIND);
+    } else {
+      alert(MESSAGES.ERROR.USER_NOT_FIND);
+    }
+  }
+
+  const updateUser = async (e) => {
+    e.preventDefault();
+    if (!name.trim()) return setErrorMsg(MESSAGES.ERROR.EMPTY_NAME);
+    if (!phone.trim()) return setErrorMsg(MESSAGES.ERROR.EMPTY_PHONE);
+    try {
+      const userUpdated = {
+        userName: name,
+        userPhone: phone
+      };
+      await setDoc(doc(db, "agenda", userId), userUpdated)
+        .then(alert(MESSAGES.USER_UPDATED));
+    } catch (error) {
+      console.log(error);
+    }
+    setUserId("");
+    setName("");
+    setPhone("");
+    setEditMode(false);
   }
 
   useEffect(() => {
@@ -53,7 +90,7 @@ function App() {
       <div className="row">
         <div className="col">
           <h2 className="text-center">Formulario de usuarios</h2>
-          <form className="form-group" onSubmit={addUser}>
+          <form className="form-group" onSubmit={editMode ? updateUser : addUser}>
             <input
               className="form-control"
               placeholder="Nombre Apellido"
@@ -68,11 +105,23 @@ function App() {
               onChange={(e) => { setPhone(e.target.value); }}
               value={phone}
             />
-            <input
-              type="submit"
-              value="Registrar"
-              className="btn btn-dark btn-block w-100"
-            />
+            {
+              editMode
+                ? (
+                  <input
+                    type="submit"
+                    value="Editar"
+                    className="btn btn-dark btn-block w-100"
+                  />
+                )
+                : (
+                  <input
+                    type="submit"
+                    value="Registrar"
+                    className="btn btn-dark btn-block w-100"
+                  />
+                )
+            }
           </form>
           {
             errorMsg
@@ -86,10 +135,17 @@ function App() {
             users.length !== 0
               ? (users.map(item => (
                 <li className="list-group-item d-flex justify-content-between" key={item.id}>
-                  {item.userName} -- {item.userPhone}
-                  <button className="btn btn-danger float-right" onClick={(id)=>{deleteUser(item.id)}}>
-                    Borrar
-                  </button>
+                  <div>
+                    {item.userName} -- {item.userPhone}
+                  </div>
+                  <div>
+                    <button className="btn btn-info mx-2" onClick={(id) => { getUser(item.id) }}>
+                      Actualizar
+                    </button>
+                    <button className="btn btn-danger" onClick={(id) => { deleteUser(item.id) }}>
+                      Borrar
+                    </button>
+                  </div>
                 </li>
               )))
               : (<span>Sin usuarios registrados</span>)
--- src/MESSAGES.js
+++ src/MESSAGES.js
@@ -1,8 +1,13 @@
 export const MESSAGES = {
   USER_CREATED: "Usuario creado",
+  USER_UPDATED: "Usuario modificado",
+  USER_DELETED: "Usuario eliminado",
+  USER_FIND: "Usuario encontrado",
   ERROR: {
     DEFAULT: "Error en sus datos",
     EMPTY_NAME: "El campo 'nombre' esta vacio",
     EMPTY_PHONE: "El campo 'telefono' esta vacio",
+    USER_NOT_CREATED: "Usuario no creado",
+    USER_NOT_FIND: "Usuario no encontrado",
   },
 };
